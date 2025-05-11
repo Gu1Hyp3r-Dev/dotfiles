08d7cc3f593763f7c46db805546e2c1441fadb70
Gu1Hyp3r-Dev <gu1hyp3r.main@gmail.com>
2025-05-11 15:21:23 +0000

    Changed a default font in emacs config

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -325,7 +325,7 @@ one, an error is signaled."
 ;; This sets the default font on all graphical frames created after restarting E...
 ;; Does the same thing as 'set-face-attribute default' above, but emacsclient fo...
 ;; are not right unless I also add this method of setting the default font.
-(add-to-list 'default-frame-alist '(font . "JetBrains Mono-11"))
+(add-to-list 'default-frame-alist '(font . "CaskaydiaCove Nerd Font Mono"))
 
 ;; Uncomment the following line if line spacing needs adjusting.
 (setq-default line-spacing 0.12)
