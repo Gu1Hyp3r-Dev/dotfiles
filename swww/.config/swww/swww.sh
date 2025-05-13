1752af1fb7a8d69b655364e026aa375f9ee7b449
Gu1Hyp3r-Dev <gu1hyp3r.main@gmail.com>
2025-05-13 02:59:20 +0000

    Added hyprlock (doesnt have wallpaper for now)

--- swww/.config/swww/swww.sh
+++ swww/.config/swww/swww.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-WALLPAPER_DIR="$HOME/.config/swww/wallpapers/"
+WALLPAPER_DIR="$HOME/.config/wallpapers/"
+
 
 if [ ! -d "$WALLPAPER_DIR" ]; then
     echo "Error: Wallpaper directory '$WALLPAPER_DIR' not found." >&2
     exit 1
 fi
 
-CURRENT_WALLPAPER=$(swww query | grep -oP 'image: \K.*' | head -n 1 | xargs basename)
+CURRENT_WALLPAPER="$( ls ${WALLPAPER_DIR}active-wallpaper )"
+CURRENT_WALLPAPER_DIR="$WALLPAPER_DIR$CURRENT_WALLPAPER"
 
-echo "$CURRENT_WALLPAPER"
+
+echo "Current wallpaper: $CURRENT_WALLPAPER"
+echo "Current wallpaper dir: $CURRENT_WALLPAPER_DIR"
 
 TOTAL_WALLPAPERS=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) | wc -l)
 
-echo "$TOTAL_WALLPAPERS"
+echo "Total wallpapers: $TOTAL_WALLPAPERS"
 
 if [ "$TOTAL_WALLPAPERS" -eq 1 ] && [ -n "$CURRENT_WALLPAPER" ]; then
     echo "No other wallpapers available in '$WALLPAPER_DIR'." >&2
@@ -23,7 +27,7 @@ fi
 while true; do
     NEW_WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) | shuf -n 1 | xargs basename)
 
-    echo "$NEW_WALLPAPER"
+    echo "New wallpaper: $NEW_WALLPAPER"
   
     if [ -z "$CURRENT_WALLPAPER" ]; then
         break
@@ -33,11 +37,12 @@ while true; do
         break
     fi
   
-    sleep 0.1
 done
 
 NEW_WALLPAPER_DIR="$WALLPAPER_DIR$NEW_WALLPAPER"
 
-echo "$NEW_WALLPAPER_DIR"
+ln -sf "$NEW_WALLPAPER_DIR" "${WALLPAPER_DIR}active-wallpaper"
+
+echo "New wallpaper dir: $NEW_WALLPAPER_DIR"
 
 swww img --transition-fps 165 --transition-duration 1.5 "$NEW_WALLPAPER_DIR" --transition-type any
